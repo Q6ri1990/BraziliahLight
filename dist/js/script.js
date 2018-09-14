f0b5813ce74ba33f0f95e190bb46d163458e090c
Ali Al-Sahli <alial-sahli@Alis-MacBook-Pro.local>
2018-09-14 17:08:24 +0300

    Trying to resolve the caching issue

--- dist/js/script.js
+++ dist/js/script.js
@@ -63,8 +63,7 @@ const alphaScoreBoard = document.getElementById("alphaScoreList");
 const bravoScoreBoard = document.getElementById("bravoScoreList");
 const alphaStatus = document.getElementById("alphaStatus");
 const bravoStatus = document.getElementById("bravoStatus");
-var alphaFirstSuper = true,
-    bravoFirstSuper = true;
+var alphaFirstSuper = true, bravoFirstSuper = true;
 
 // create team objects
 const alpha = new Team("Alpha", 0);
@@ -143,93 +142,3 @@ function checkSuperState() {
         }
     }
 }
-
-/**
- * Java Code
- * private void updateStatus() {
-
-        boolean play = false, omg = false;
-        alphaScore.setText(alpha.getScore() + "");
-        bravoScore.setText(bravo.getScore() + "");
-        alphaTrace.setText(alpha.getPreviousScore());
-        bravoTrace.setText(bravo.getPreviousScore());
-
-
-        if (alpha.getScore() >= 2000 && bravo.getScore() >= 2000) {
-            if (alpha.getScore() > bravo.getScore()) {
-                alphaStatus.setText(getText(R.string.Win));
-                alphaStatus.setTextColor(Color.parseColor("#006400"));
-                bravoStatus.setText(getText(R.string.Lose));
-            } else if (alpha.getScore() == bravo.getScore()) {
-                alphaStatus.setText(getText(R.string.Draw));
-                bravoStatus.setText(getText(R.string.Draw));
-            } else {
-                alphaStatus.setText(getText(R.string.Lose));
-                bravoStatus.setText(getText(R.string.Win));
-                bravoStatus.setTextColor(Color.parseColor("#006400"));
-
-            }
-        } else if (alpha.getScore() >= 2000) {
-            alphaStatus.setText(getText(R.string.Win));
-            bravoStatus.setText(getText(R.string.Lose));
-            alphaStatus.setTextColor(Color.parseColor("#006400"));
-        } else if (bravo.getScore() >= 2000) {
-            alphaStatus.setText(getText(R.string.Lose));
-            bravoStatus.setText(getText(R.string.Win));
-            bravoStatus.setTextColor(Color.parseColor("#006400"));
-        } else {
-
-
-            if (alpha.getScore() < 1000) {
-                alphaFirstSuper = true;
-                alphaStatus.setText(R.string.initialStatus);
-            } else if (alpha.isSuper()) {
-                alphaStatus.setText(getString(R.string.Super));
-                alphaStatus.setTextColor(Color.RED);
-                if (alphaFirstSuper) {
-                    play = true;
-                    alphaFirstSuper = false;
-                }
-            }
-
-            if (bravo.getScore() < 1000) {
-                bravoStatus.setText(R.string.initialStatus);
-                bravoFirstSuper = true;
-            } else if (bravo.isSuper()) {
-                bravoStatus.setText(getString(R.string.Super));
-                bravoStatus.setTextColor(Color.RED);
-                if (bravoFirstSuper) {
-                    play = true;
-                    bravoFirstSuper = false;
-                }
-            }
-        }
-        if (bravo.isSuper() && alpha.isSuper()) {
-            if(bravo.getScore() == alpha.getScore())
-            {
-                omg = true;
-            }else {
-                int diff = Math.max(alpha.getScore(), bravo.getScore()) - Math.min(alpha.getScore(), bravo.getScore());
-                int margin = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getInt("margin", 200) ;
-                omg = diff <= margin && diff > 0;
-            }
-        }
-
-        if (omg) {
-            playSound(R.raw.omg);
-        } else if (play) {
-            playSound(R.raw.suuper);
-        }
-
-
-    }
-
-    private void playSound(int s) {
-        sound = MediaPlayer.create(getApplicationContext(), s);
-        sound.start();
-
-    }
-
- * 
- * 
- */
