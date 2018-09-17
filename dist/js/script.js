bdfc86df0d94f60970a7b7c2c32a91c640f7192a
Ali Al-Sahli <alial-sahli@Alis-MacBook-Pro.local>
2018-09-17 19:25:57 +0300

    Downloaded the image instead of getting it from the site, and added an iOS check to toggle the add to home screen message

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,11 +1,27 @@
+// Initiate the service worker
 var sw = '../dist/sw_cached_site.js';
 // Make sure sw are supported
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker
             .register(sw)
-            .then(reg => console.log('Service Worker: Registered (Pages):'+sw))
+            .then(reg => console.log('Service Worker: Registered (Pages):' + sw))
             .catch(err => console.log(`Service Worker: Error: ${err}`));
+    });
+}
+
+// Detects if device is on iOS 
+const isIos = () => {
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    return /iphone|ipad|ipod/.test(userAgent);
+}
+// Detects if device is in standalone mode
+const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+// Checks if should display install popup notification:
+if (isIos() && !isInStandaloneMode()) {
+    this.setState({
+        showInstallMessage: true
     });
 }
 
@@ -64,7 +80,8 @@ const alphaScoreBoard = document.getElementById("alphaScoreList");
 const bravoScoreBoard = document.getElementById("bravoScoreList");
 const alphaStatus = document.getElementById("alphaStatus");
 const bravoStatus = document.getElementById("bravoStatus");
-var alphaFirstSuper = true, bravoFirstSuper = true;
+var alphaFirstSuper = true,
+    bravoFirstSuper = true;
 
 // create team objects
 const alpha = new Team("Alpha", 0);
@@ -79,8 +96,8 @@ function calculateScores() {
     bravo.addScore(bravoInput.value);
     updateScoreBoards();
 
-    alphaInput.value="";
-    bravoInput.value="";
+    alphaInput.value = "";
+    bravoInput.value = "";
 }
 
 function deletePreviousScore() {
@@ -100,25 +117,25 @@ function checkSuperState() {
     var play = false;
     if (alpha.score >= 2000 && bravo.score >= 2000) {
         if (alpha.score > bravo.score) {
-            alphaStatus.innerText="Win";
+            alphaStatus.innerText = "Win";
             alphaStatus.style.color = "#006400";
-            bravoStatus.innerText="Lose";
+            bravoStatus.innerText = "Lose";
         } else if (alpha.score == bravo.score) {
-            alphaStatus.innerText="Draw";
-            bravoStatus.innerText="Draw";
+            alphaStatus.innerText = "Draw";
+            bravoStatus.innerText = "Draw";
         } else {
-            alphaStatus.innerText="Lose";
-            bravoStatus.innerText="Win";
+            alphaStatus.innerText = "Lose";
+            bravoStatus.innerText = "Win";
             bravoStatus.style.color = "#006400";
 
         }
     } else if (alpha.score >= 2000) {
-        alphaStatus.innerText="Win";
-        bravoStatus.innerText="Lose";
+        alphaStatus.innerText = "Win";
+        bravoStatus.innerText = "Lose";
         alphaStatus.style.color = "#006400";
     } else if (bravo.score >= 2000) {
-        alphaStatus.innerText="Lose";
-        bravoStatus.innerText="Win";
+        alphaStatus.innerText = "Lose";
+        bravoStatus.innerText = "Win";
         bravoStatus.style.color = "#006400";
     } else {
         if (alpha.isSuper()) {
@@ -130,20 +147,20 @@ function checkSuperState() {
             }
         } else {
             alphaStatus.innerText = "";
-            alphaStatus.className="";
+            alphaStatus.className = "";
         }
 
 
         if (bravo.isSuper()) {
             bravoStatus.innerText = "Super";
-            bravoStatus.className="superState";
+            bravoStatus.className = "superState";
             if (bravoFirstSuper) {
                 play = true;
                 bravoFirstSuper = false;
             }
         } else {
             bravoStatus.innerText = "";
-            bravoStatus.className="";
+            bravoStatus.className = "";
         }
     }
 }
