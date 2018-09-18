2c1634828bb411bcf95d504580f81afa56452e03
Ali Al-Sahli <alial-sahli@Alis-MacBook-Pro.local>
2018-09-18 22:22:32 +0300

    added installation hint

--- dist/js/script.js
+++ dist/js/script.js
@@ -13,17 +13,17 @@ if ('serviceWorker' in navigator) {
 // Detects if device is on iOS 
 const isIos = () => {
     const userAgent = window.navigator.userAgent.toLowerCase();
-    return /iphone|ipad|ipod/.test(userAgent);
-}
-// Detects if device is in standalone mode
-const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
-
-// Checks if should display install popup notification:
-if (isIos() && !isInStandaloneMode()) {
-    this.setState({
-        showInstallMessage: true
-    });
-}
+    return /iphone|ipad|ipod/.test( userAgent );
+  }
+  // Detects if device is in standalone mode
+  const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+  
+  // Checks if should display install popup notification:
+  if (isIos() && !isInStandaloneMode()) {
+    // this is using react for some reason
+    //this.setState({ showInstallMessage: true });
+    document.getElementById("iosInstallHint").className="installPopup";
+  }
 
 // define the Team class
 class Team {
